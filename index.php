c2bf29880ef33b86efdfab03642efd54bbaca795
kushan2k <kushangayantha001@gmail.com>
2022-11-28 13:31:24 +0530

    first commit/files sturcture created

---
+++ index.php
@@ -0,0 +1,16 @@
+<html lang="en">
+<head>
+  <?php include './includes/header.php'?>
+  <title>FoodsFly-Home</title>
+</head>
+<body>
+
+  
+
+
+
+
+
+  <?php include './includes/scripts.php'?>
+</body>
+</html>
