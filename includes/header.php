2a40b607e79573bc6e94d87025685365c753ce78
kushan2k <kushangayantha001@gmail.com>
2022-11-28 14:22:08 +0530

    custom navabar created

--- includes/header.php
+++ includes/header.php
@@ -1,4 +1,6 @@
 <meta charset="UTF-8">
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css" integrity="sha512-9usAa10IRO0HhonpyAIVpjrylPvoDwiPUiKdWk5t3PyolY1cOd4DSE0Ga+ri4AuTroPR5aQvXU9xC6qOPnzFeg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
--- index.php
+++ index.php
@@ -2,15 +2,30 @@
 <head>
   <?php include './includes/header.php'?>
   <title>FoodsFly-Home</title>
+  <link rel="stylesheet" href="./assets/css/navbar.css">
 </head>
-<body>
+<body class="">
+
+<nav class="container-fluid  px-4 bg-dark d-flex flex-row navbar-div justify-content-end">
+  <ul class=" navbar d-none d-sm-flex">
+    <li class="nav_item" >Home</li>
+    <li class="nav_item">Menu</li>
+    <li class="nav_item">Orders</li>
+    <li class="nav_item">About us</li>
+    <li class="nav_item">Contact us</li>
+    
+  </ul>
+  <button class="d-sm-none bg-transparent border-0"><i class="fa-solid fa-bars text-white"></i></button>
+</nav>
+
+  
 
   
 
 
 
 
-
+  <script src="./assets/js/navbar.js"></script>
   <?php include './includes/scripts.php'?>
 </body>
 </html>
