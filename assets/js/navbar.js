bffa03622862428bd2209da9b77efac3af97a4a5
kushan2k <kushangayantha001@gmail.com>
2022-11-29 12:04:41 +0530

    navbar created/fully functional

---
+++ assets/js/navbar.js
@@ -0,0 +1,41 @@
+document.addEventListener('DOMContentLoaded',()=>{
+  let navbarIconBtns=document.querySelectorAll('.icon-btn')
+
+
+  window.onresize=()=>{
+    if(window.screen.width>576){
+      let popup=document.querySelector('.navbar_popup')
+      if(popup){
+        popup.classList.remove('navbar_popup')
+        popup.classList.remove('float-box')
+        popup.classList.add('d-none')
+        popup.classList.add('navbar')
+        document.querySelector('.hide').classList.add('d-none')
+        document.querySelector('.popup').classList.remove('d-none')
+        return 
+      }
+    }
+  }
+
+  navbarIconBtns.forEach(btn=>{
+    btn.addEventListener('click',e=>{
+      let navbox=document.querySelector('#navbar')
+      if(e.target.classList.contains("popup")){
+        navbox.classList.remove('d-none')
+        navbox.classList.remove('navbar')
+        navbox.classList.add('float-box')
+        navbox.classList.add('navbar_popup')
+        e.target.classList.add('d-none')
+        document.querySelector('.hide').classList.remove('d-none')
+
+      }else if(e.target.classList.contains('hide')){
+        navbox.classList.remove('navbar_popup')
+        e.target.classList.add('d-none')
+        document.querySelector('.popup').classList.remove('d-none')
+        navbox.classList.remove('float-box')
+        navbox.classList.add('d-none')
+        navbox.classList.add('navbar')
+      }
+      })
+  })
+})
--- index.php
+++ index.php
@@ -3,19 +3,22 @@
   <?php include './includes/header.php'?>
   <title>FoodsFly-Home</title>
   <link rel="stylesheet" href="./assets/css/navbar.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 </head>
-<body class="">
+<body class=" position-relative">
 
 <nav class="container-fluid  px-4 bg-dark d-flex flex-row navbar-div justify-content-end">
-  <ul class=" navbar d-none d-sm-flex">
-    <li class="nav_item" >Home</li>
+  <ul id="navbar" class="navbar d-none   d-sm-flex "> <!-- put navbar class -->
+    <li class="nav_item " >Home</li>
     <li class="nav_item">Menu</li>
     <li class="nav_item">Orders</li>
     <li class="nav_item">About us</li>
     <li class="nav_item">Contact us</li>
     
   </ul>
-  <button class="d-sm-none bg-transparent border-0"><i class="fa-solid fa-bars text-white"></i></button>
+  <button class="d-sm-none bg-transparent border-0 icon-btn popup fa-solid fa-bars text-white"></button>
+
+  <button class="d-sm-none bg-transparent border-0 icon-btn d-none hide fa-solid fa-xmark text-white"></button>
 </nav>
 
   
