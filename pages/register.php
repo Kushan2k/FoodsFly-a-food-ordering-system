0add42458e12519323dbf5b64abd784eb63f084b
kushan2k <kushangayantha001@gmail.com>
2022-12-01 16:14:56 +0530

    footer added

--- pages/register.php
+++ pages/register.php
@@ -105,6 +105,20 @@
     </div>
   </section>
 
+  <div class="container">
+    <hr>
+    <footer class="py-3 my-4">
+      <ul class="nav justify-content-center border-bottom pb-3 mb-3">
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-dark">Home</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-dark">Features</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-dark">Pricing</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-dark">FAQs</a></li>
+        <li class="nav-item"><a href="#" class="nav-link px-2 text-dark">About</a></li>
+      </ul>
+      <p class="text-center text-muted">© 2022 FoodsFly, Org</p>
+    </footer>
+  </div>
+
 
   
   <?php include '../includes/scripts.php'?>
