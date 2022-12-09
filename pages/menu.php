0b8d345a9eaa77bc08bc31e9cc792368ff167f07
kushan2k <kushangayantha001@gmail.com>
2022-12-09 16:02:14 +0530

    filter menu and item menu created

--- pages/menu.php
+++ pages/menu.php
@@ -56,25 +56,225 @@ if(checkIsLogedIn()){
   </nav>
 
   <section class="menu_page mt-3">
-    <div class="container serach_options_box">
-      <div class="row ">    
-        <div class="col-10 mx-auto ">
-          <div class="row mx-auto">
-            <div class="col-2 m-0">
-              <p class="alert alert-danger">hello</p>
+    <!-- <div class="container py-5 my-5">
+      <div class="row text-center pb-4">
+        <div class="col-md-12">
+            <h2>explore foods and ..</h2>
+        </div>
+      </div>   
+      <div class="row">
+          <div class="col-md-12">
+              <div class="card" style="border: none;">
+                  <div class="card-body" >
+                    <div class="row">
+                      <div class="col-md-4">
+                        <div class="form-group ">
+                          <input type="text" class="form-control">
+                        </div>
+                      </div>
+                      <div class="col-md-2">
+                        <div class="form-group ">
+                          <select id="inputState" class="form-control" >
+                            <option selected>Sort by </option>
+                            <option>BMW</option>
+                            <option>Audi</option>
+                            <option>Maruti</option>
+                            <option>Tesla</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="col-md-2">
+                        <div class="form-group ">
+                          <select id="inputState" class="form-control" >
+                            <option selected>... Select Model...</option>
+                            <option>City</option>
+                            <option>Jazz</option>
+                            <option>Brio</option>
+                            
+                          </select>
+                        </div>
+                      </div>
+                      <div class="col-md-2">
+                        <div class="form-group ">
+                          <select id="inputState" class="form-control" >
+                            <option selected>... Select Location...</option>
+                            <option>New Delhi</option>
+                            <option>Banglore</option>
+                            <option>Chennai</option>
+                            <option>Jaipur</option>
+                          </select>
+                        </div>
+                      </div>
+                      <div class="col-md-2">
+                        <div class="form-group ">
+                          <select id="inputState" class="form-control" >
+                            <option selected>... Select Budget...</option>
+                            <option>1 Lac-5 Lac</option>
+                            <option>5 Lac-10 Lac</option>
+                            <option>10 Lac-15 Lac</option>
+                            <option>15 Lac-20 Lac</option>
+                            <option>20 Lac-25 Lac</option>
+                            <option>25 Lac & Above</option>
+                          </select>
+                        </div>
+                      </div>
+                    
+                    </div>
+                  </div>
+              </div> 
+          </div>
+      </div>
+    </div> -->
+
+    <style>
+      .card-header{
+        font-weight: 700;
+      }
+      .list-group{
+        margin-left: 10px;
+      }
+      .list-group-item:hover{
+        cursor: pointer;
+        background-color: #dedede;
+      }
+    </style>
+
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-lg-3 col-md-3 col-3 ">
+
+          <div class="card" style="border: none;">
+            <div class="card-header">
+              Catogory
             </div>
-            <div class="col-6 m-0">
-              <p class="alert alert-warning">hello</p>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">Breakfirst</li>
+              <li class="list-group-item">Lunch</li>
+              <li class="list-group-item">Dinner</li>
+              <li class="list-group-item">Snacks</li>
+            </ul>
+          </div>
+
+          <div class="card mt-3" style="border: none;">
+            <div class="card-header">
+              Price Range(LKR)
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">400-700</li>
+              <li class="list-group-item">700-1500</li>
+              <li class="list-group-item">1500-Up</li>
+            </ul>
+          </div>
+
+          <div class="card mt-3" style="border: none;">
+            <div class="card-header">
+              something else
+            </div>
+            <ul class="list-group list-group-flush">
+              <li class="list-group-item">An item</li>
+              <li class="list-group-item">A second item</li>
+              <li class="list-group-item">A third item</li>
+            </ul>
+          </div>
+
+      
+        </div>
+        <div class="col-lg-9 col-md-9 col-9">
+
+          <div class="row"> 
+            <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                  <img src="https://mdbootstrap.com/img/new/standard/nature/111.jpg" class="img-fluid" />
+                  <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the
+                    card's content.
+                  </p>
+                  <a href="#!" class="btn btn-primary">Button</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                  <img src="https://mdbootstrap.com/img/new/standard/nature/111.jpg" class="img-fluid" />
+                  <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the
+                    card's content.
+                  </p>
+                  <a href="#!" class="btn btn-primary">Button</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                  <img src="https://mdbootstrap.com/img/new/standard/nature/111.jpg" class="img-fluid" />
+                  <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the
+                    card's content.
+                  </p>
+                  <a href="#!" class="btn btn-primary">Button</a>
+                </div>
+              </div>
+            </div>
+            <div class="col-lg-4 col-md-6 mb-4">
+              <div class="card">
+                <div class="bg-image hover-overlay ripple" data-mdb-ripple-color="light">
+                  <img src="https://mdbootstrap.com/img/new/standard/nature/111.jpg" class="img-fluid" />
+                  <a href="#!">
+                    <div class="mask" style="background-color: rgba(251, 251, 251, 0.15);"></div>
+                  </a>
+                </div>
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">
+                    Some quick example text to build on the card title and make up the bulk of the
+                    card's content.
+                  </p>
+                  <a href="#!" class="btn btn-primary">Button</a>
+                </div>
+              </div>
             </div>
           </div>
+
         </div>
       </div>
     </div>
 
-    <div class="container menu-items-box">
-
-    </div>
+    
   </section>
+
+  <script>
+    document.addEventListener('DOMContentLoaded',()=>{
+      let types=document.querySelectorAll('.list-group-item');
+      
+      types.forEach((item)=>{
+        item.addEventListener('click',(e)=>{
+          alert(e.target.innerHTML)
+        })
+      })
+
+    })
+  </script>
 
  <?php include '../includes/scripts.php'?>
 </body>
